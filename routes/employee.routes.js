b089f982d2e55893bce3dd75733a8b8d9cb4d6ea
aldaine-AG <aldaineclarke1@gmail.com>
2022-06-28 09:45:27 -0500

    added page and route for viewing employees

--- routes/employee.routes.js
+++ routes/employee.routes.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const EmployeeController = require("../controllers/employee.controller.js")
 
-router.get("employees/:id", EmployeeController.getEmployee)
-router.get("employees", EmployeeController.getEmployees)
+router.get("/", EmployeeController.getAllEmployees);
+router.get("details/:id", EmployeeController.getEmployee);
+router.get("edit/:id", EmployeeController.getEditEmployeeForm);
+router.get("add", EmployeeController.getEmployeeCreationForm);
+router.post("add", EmployeeController.createEmployee);
 module.exports = router
