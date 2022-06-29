bf2a51e0c3074fc95196e0400debe4e48fe8e1f1
aldaine-AG <aldaineclarke1@gmail.com>
2022-06-29 12:10:14 -0500

    updated employee route to add delete functionality

--- routes/employee.routes.js
+++ routes/employee.routes.js
@@ -3,7 +3,8 @@ const EmployeeController = require("../controllers/employee.controller.js")
 
 router.get("/", EmployeeController.getAllEmployees);
 router.get("/details/:id", EmployeeController.getEmployee);
-router.get("/edit/:id", EmployeeController.getEditEmployeeForm);
+router.post("/details/:id", EmployeeController.updateEmployee);
+router.get("/delete/:id", EmployeeController.deleteEmployee);
 router.get("/add", EmployeeController.getEmployeeCreationForm);
 router.post("/add", EmployeeController.createEmployee);
 module.exports = router
